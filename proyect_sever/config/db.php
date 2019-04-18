ec1479a4d449008065f67c6bea07d218e438b2a0
Javier <javi_m_127@hotmail.com>
2019-04-18 18:23:26 -0300

    Se agrego la bibloteca de estilos. Javier Vivias.

---
+++ proyect_sever/config/db.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=ties.com.ar;dbname=tiesPoint',
+    'username' => 'point2019',
+    'password' => 'TiesPoint2019',
+    'charset' => 'utf8',
+
+    // Schema cache options (for production environment)
+    //'enableSchemaCache' => true,
+    //'schemaCacheDuration' => 60,
+    //'schemaCache' => 'cache',
+];
